a9103a43ab701642b25cdb87ef23cdc6b0b7ef1f
Fauzan <xyzan3585@gmail.com>
2026-01-28 02:06:41 +0700

    add jwt for authentication

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,20 +1,33 @@
 import { UnauthorizedError } from "../../utils/errors/unauthorizedError.js"
 import { UserModel } from "../user/user.model.js"
-
-const userModel = new UserModel()
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export class AuthService {
-    login = async (email, password) => {
-          let findUser = await userModel.findByEmail(email)
+  login = async (email, password) => {
+    let findUser = await UserModel.findByEmail(email)
 
-          if(!findUser){
-              throw new UnauthorizedError("Email Atau Password Salah.")
-          }
-
-          if(findUser[0].Password != password){
-            throw new UnauthorizedError("Email Atau Password Salah.")
-          }
-
-          return findUser
+    if (!findUser) {
+      throw new UnauthorizedError("Email Atau Password Salah.")
     }
+
+    let encryptedPassword = findUser[0].password
+
+    let isMatch = bcrypt.compare(password, encryptedPassword)
+
+    if (!isMatch) {
+      throw new UnauthorizedError("Email Atau Password Salah.")
+    }
+
+    let { Name, Username, Email, role } = findUser[0]
+
+    let token = jwt.sign({ email, role }, 'SECRET_KEY02')
+
+    return [
+      {
+        jwt: token,
+        user: { Name, Username, Email }
+      }
+    ]
+  }
 }
