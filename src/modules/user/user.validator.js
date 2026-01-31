c0519a752a22c0c53561fc15c2b4a7ddd06c849a
Fauzan <xyzan3585@gmail.com>
2026-01-31 19:48:53 +0700

    fix bug, sebelumnya menggunakan pengecekan isEmpty() menjadi notEmpty()

--- src/modules/user/user.validator.js
+++ src/modules/user/user.validator.js
@@ -1,29 +1,48 @@
 import { validateRequest } from "../../middlewares/validator.middleware.js"
 import { body } from "express-validator"
-export const userValidator = [
-
+export const createUserValidator = [
     body("name")
-        .isEmpty()
+        .notEmpty()
         .withMessage("Nama Tidak Boleh Kosong"),
 
     body("username")
-        .isEmpty()
+        .notEmpty()
         .withMessage("Username Tidak Boleh Kosong")
         .isLength({ max: 50 })
         .withMessage("Username Maksimal 50 karakter"),
 
     body("email")
-        .isEmpty()
+        .notEmpty()
         .withMessage("Email Tidak Boleh Kosong")
         .isEmail()
         .withMessage("Email Tidak Valid"),
 
     body("password")
-        .isEmpty()
+        .notEmpty()
         .withMessage("Password Tidak Boleh Kosong")
         .isLength({ min: 5 })
         .withMessage("Password Minimal 5 karakter"),
 
     validateRequest
 
+]
+
+export const updatePforileValidator = [
+      body("newName")
+        .notEmpty()
+        .withMessage("Nama Tidak Boleh Kosong"),
+
+    body("newUsername")
+        .notEmpty()
+        .withMessage("Username Tidak Boleh Kosong")
+        .isLength({ max: 50 })
+        .withMessage("Username Maksimal 50 karakter"),
+
+    body("newEmail")
+        .notEmpty()
+        .withMessage("Email Tidak Boleh Kosong")
+        .isEmail()
+        .withMessage("Email Tidak Valid"),
+
+        validateRequest
 ]
