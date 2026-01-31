36d74beb5a790c082ad2e4ab217db48bbe1a97e0
Fauzan <xyzan3585@gmail.com>
2026-01-31 19:46:27 +0700

    Menambahkan async handler (try & finally)

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -1,60 +1,111 @@
 import { connection } from "../../../config/db/config.db.js"
 export class UserModel {
-    static create = async function (name, username, email, password, avatar) {
-        let main = await connection()
-        let query = 'INSERT INTO t_users VALUES(?, ?, ?, ?, ?, ?, ?, ?)'
-        let [QueryResult] = await main.query(query, [null, name, username, email, password, avatar, null, null])
-        return QueryResult
+    static create = async function (name, username, email, password) {
+        let main = null
+        try {
+            main = await connection()
+            let query = 'INSERT INTO t_users(Name, Username, Email, Password) VALUES(?, ?, ?, ?)'
+            let [QueryResult] = await main.query(query, [name, username, email, password])
+            return QueryResult
+        } finally {
+            await main.end()
+        }
     }
 
     static findAll = async function () {
-        let main = await connection()
-        let query = 'SELECT * FROM t_users WHERE Role = Teacher'
-        let [QueryResult] = await main.query(query)
-        return QueryResult
+        let main = null
+        try {
+            main = await connection()
+            let query = 'SELECT * FROM t_users WHERE Role = "Teacher"'
+            let [QueryResult] = await main.query(query)
+            return QueryResult
+        } finally {
+            await main.end()
+        }
     }
 
-    static update = async function (idUser, newName, newUsername, newEmail) {
-        let main = await connection()
-        let query = 'UPDATE t_users SET Name = ?, Username = ?, Email = ?'
-        let [QueryResult] = await main.query(query, [newName, newUsername, newEmail, idUser])
-        return QueryResult
+    static update = async function (userId, newName, newUsername, newEmail, password) {
+        let main = null
+        try {
+            main = await connection()
+            let query = 'UPDATE t_users SET Name = ?, Username = ?, Email = ?, Password = ? WHERE Id = ?'
+            let [QueryResult] = await main.query(query, [newName, newUsername, newEmail, password, userId])
+            return QueryResult
+        } finally {
+            await main.end()
+        }
     }
 
-    static delete = async function (idUser) {
-        let main = await connection()
-        let query = 'DELETE FROM t_users WHERE id = ?'
-        let [QueryResult] = await main.query(query, [idUser])
-        return QueryResult
+    static delete = async function (userId) {
+        let main = null
+        try {
+            main = await connection()
+            let query = 'DELETE FROM t_users WHERE id = ?'
+            let [QueryResult] = await main.query(query, [userId])
+            return QueryResult
+        } finally {
+            await main.end()
+        }
     }
 
-   static updateStatus = async function (idUser, newStatus) {
-        let main = await connection()
-        let query = 'UPDATE t_users SET Status = ? WHERE id = ?'
-        let [QueryResult] = await main.query(query, [newStatus, idUser])
-        return QueryResult
+    static updateStatus = async function (userId, newStatus) {
+        let main = null
+        try {
+            main = await connection()
+            let query = 'UPDATE t_users SET Status = ? WHERE Id = ?'
+            let [QueryResult] = await main.query(query, [newStatus, userId])
+            return QueryResult
+
+        } finally {
+            await main.end()
+        }
     }
 
-   static updatePassword = async function (idUser, newPassword) {
-        let main = await connection()
-        let query = 'UPDATE t_users SET Password = ? WHERE id = ?'
-        let [QueryResult] = await main.query(query, [newPassword, idUser])
-        return QueryResult
+    static updatePassword = async function (userId, newPassword) {
+        let main = null
+        try {
+            main = await connection()
+            let query = 'UPDATE t_users SET Password = ? WHERE Id = ?'
+            let [QueryResult] = await main.query(query, [newPassword, userId])
+            return QueryResult
+        } finally {
+            await main.end()
+        }
     }
 
-   static updateAvatar = async function (idUser, newAvatar) {
-        let main = await connection()
-        let query = 'UPDATE t_users SET Avatar = ? WHERE id = ?'
-        let [QueryResult] = await main.query(query, [newAvatar, idUser])
-        return QueryResult
+    static updateAvatar = async function (userId, newAvatar) {
+        let main = null
+        try {
+            main = await connection()
+            let query = 'UPDATE t_users SET Avatar = ? WHERE Id = ?'
+            let [QueryResult] = await main.query(query, [newAvatar, userId])
+            return QueryResult
+        } finally {
+            await main.end()
+        }
     }
 
-   static findByEmail = async function (email) {
-        let main = await connection()
-        let query = 'Select * FROM t_users WHERE Email = ?'
-        let [QueryResult] = await main.query(query, [email])
-        return QueryResult
+    static findByEmail = async function (email) {
+        let main = null
+        try {
+            main = await connection()
+            let query = 'Select * FROM t_users WHERE Email = ?'
+            let [QueryResult] = await main.query(query, [email])
+            return QueryResult
+        } finally {
+            await main.end()
+        }
     }
 
-    //recap
+    static findById = async function (userId) {
+        let main = null
+        try {
+            const query = 'SELECT * FROM t_users WHERE Id = ?'
+            main = await connection()
+            let [QueryResult] = await main.query(query, [userId])
+            return QueryResult
+        } finally {
+            await main.end()
+        }
+    }
 }
