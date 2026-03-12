24bf420d24cd09471978776682f64d2094a211ee
Fauzan <xyzan3585@gmail.com>
2026-03-12 19:18:14 +0700

    Mengimplementasikan penggunaan fungsi runQuery refactoring

--- src/modules/class/class.model.js
+++ src/modules/class/class.model.js
@@ -1,51 +1,54 @@
-import { connection } from "../../../config/db/config.db.js"
-
+import { runQuery } from '../../utils/tryCatch.wrapper.js'
 export class ClassModel {
-    static create = async function (name, majorId, level) {
-        let main = null
-        try {
-            const query = 'INSERT INTO t_classes(Name, Id_major, Level) VALUES(?, ?, ?)'
-            main = await connection()
-            let [QeuryResult] = await main.query(query, [name, majorId, level])
-            return QeuryResult
-        } finally {
-            await main.end()
+
+    static async #findBy(criteria) {
+        const allowedKeys = { 'id_major': 't_majors.id = ?', 'id_class': 't_class.id = ?', 'grade': 't_class.grade = ?' }
+
+        let conditions = []
+        let values = []
+
+        let query = `SELECT t_class.id, t_class.grade, t_major.name, t_class.name FROM t_class INNER JOIN       
+                     t_majors ON t_majors.id = t_class.id_major`
+
+        Object.keys(criteria).forEach(key => {
+            if (key in allowedKeys) {
+                conditions.push(allowedKeys[key])
+                values.push(criteria[key])
+            } else {
+                throw Error(`${key}, tidak cocok`)
+            }
+        })
+
+        if (conditions.length > 0) {
+            query += ` WHERE ` + conditions.join(' AND ')
         }
+
+        let result = await runQuery(query, values)
+        return result
     }
 
-    static update = async function (classId, newName, newMajorId, newLevel) {
-        let main = null
-        try {
-            const query = 'UPDATE t_classes SET Name = ?, majorId = ?, Level = ? WHERE Id = ?'
-            main = await connection()
-            let [QeuryResult] = await main.query(query, [newName, newMajorId, newLevel, classId])
-            return QeuryResult
-        } finally {
-            await main.end()
-        }
+    static create = async function (name, majorId, grade) {
+        const query = 'INSERT INTO t_class(name, id_major, grade) VALUES(?, ?, ?)'
+        let result = await runQuery(query, [name, majorId, grade])
+        return result.insertId
     }
 
-    static findAll = async function () {
-        let main = null
-        try {
-            const query = 'SELECT * FROM t_classes'
-            main = await connection()
-            let [QeuryResult] = main.query()
-            return QeuryResult
-        } finally {
-            await main.end()
-        }
+    static update = async function (id_class, name, id_major, grade) {
+        const query = 'UPDATE t_class SET name = ?, majorId = ?, grade = ? WHERE Id = ?'
+        let result = await runQuery(query, [name, id_major, grade, id_class])
+        return result
     }
 
-    static delete = async function (classId) {
-        let main = null
-        try {
-            const query = 'DELETE FROM t_classes WHERE Id = ?'
-            main = await connection()
-            let [QeuryResult] = await main.query(query, [classId])
-            return QeuryResult
-        } finally {
-            await main.end()
-        }
+    static findAll = async function ({ criteria }) {
+        let query = `SELECT t_class.id, t_class.grade, t_major.name, t_class.name FROM t_class INNER JOIN       
+                     t_majors ON t_majors.id = t_class.id_major`
+        let result = await runQuery(query, [...criteria])
+        return result
+    }
+
+    static delete = async function (id_class) {
+        const query = 'DELETE FROM t_class WHERE id = ?'
+        await runQuery(query, id_class)
+        return null
     }
 }
